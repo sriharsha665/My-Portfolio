5477a0ce85a6dde56caa3039407d874511976b4c
SRI HARSHA <146829389+sriharsha665@users.noreply.github.com>
2025-02-05 16:34:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Python Developer", "Frontend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+// JavaScript to fix 100vh issue on mobile browsers
